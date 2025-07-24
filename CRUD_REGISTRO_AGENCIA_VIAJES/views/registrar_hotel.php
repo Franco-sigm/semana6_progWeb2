e1fe4b4a0f340ee34884db4dcd0430f9f03cbcba
Franco-sigm <francoherrera.30@outlook.com>
2025-07-24 16:38:06 -0400

    Update registrar_hotel.php
    Ruta styles/style.css

--- CRUD_REGISTRO_AGENCIA_VIAJES/views/registrar_hotel.php
+++ CRUD_REGISTRO_AGENCIA_VIAJES/views/registrar_hotel.php
@@ -5,8 +5,8 @@
   <title>Registrar Hotel</title>
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free/css/all.min.css">
-  <link rel="stylesheet" href="style.css">
-   <script src="js/validaciones.js"></script>
+  <link rel="stylesheet" href="styles/style.css">
+  <script src="js/validaciones.js"></script>
 
 </head>
 <body>
